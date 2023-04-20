f50c9cdb41bed6bcdfb6e9f142ac82f23483219c
aluamark <aluamark@gmail.com>
2023-04-20 17:23:39 +0800

    removed adsense

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata = {
 	title: "panahon",
@@ -9,13 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<Script
-				id="Adsense-id"
-				data-ad-client="ca-pub-9001845428597860"
-				async="true"
-				strategy="beforeInteractive"
-				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-			/>
 			<body className="font-mono dark:bg-zinc-800">{children}</body>
 		</html>
 	);
